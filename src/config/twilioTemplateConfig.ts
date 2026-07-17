f2aae839827d9090afe1cbb9d65bc3b56213247b
sarangnethority <sarangc.nethority@gmail.com>
2026-07-17 16:58:33 +0530

    update endding and ended

--- src/config/twilioTemplateConfig.ts
+++ src/config/twilioTemplateConfig.ts
@@ -55,8 +55,8 @@ export const TWILIO_TEMPLATE_MAPPINGS: Record<string, string> = {
   "copy_executivesummarydaily":'HXeb73ec3c633a5a02c02ebdc727c05cb6',
   "executivesummaryweeklymonthly":'HX376fa75f8be59b60ccb713d852d185d2',
   "trial_after_expiry_reminder_new":'HX5870797abc2f3aef9c8d863ac05d2fb0',
-  "trial_period_ended":'HXb739a1f343bbc5c2c48334269ba5b743',
   "trial_period_ending":'HX34edeca12f91002c41bf0d80f733513d',
+  "trial_period_ended_new":'HXddd74bad4ad05882994e620c85f2723d',
 };
 
 export function getTwilioTemplateId(templateName: string): string | null {
